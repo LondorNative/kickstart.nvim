a426fbaaeb4b10d1af1c9e681a72a21c95d7fe1e
Seth Hendrix <sethhendrix0629@gmail.com>
2024-10-07 20:54:53 -0400

    updated rust_analyzer settings

--- init.lua
+++ init.lua
@@ -620,7 +620,29 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         --pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          on_attach = function(client, bufnr)
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          end,
+          settings = {
+            ['rust-analyzer'] = {
+              imports = {
+                granularity = {
+                  group = 'module',
+                },
+                prefix = 'self',
+              },
+              cargo = {
+                buildScripts = {
+                  enable = true,
+                },
+              },
+              procMacro = {
+                enable = true,
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
